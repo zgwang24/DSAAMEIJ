a16dd7216d7f669055dc0b1d19f9c5fb23fc0d42
wangzhiguo <zgwang24@163.com>
2017-02-25 20:29:24 +0800

    create a new project about data structure and algorithms

---
+++ src/Recursion/Recursion.java
@@ -0,0 +1,34 @@
+/**
+ * @author zgwang
+ *
+ */
+package Recursion;
+public class Recursion{
+	// 汉诺塔谜题（递归实现）
+	public static void TowersOfHanoi(int n, char from, char to, char aux){
+		if(n == 1){
+			System.out.println(from + " move to " + to);
+		}else if(n < 1){
+			System.out.println("Please enter a bigger integer");
+		}else{
+			TowersOfHanoi(n - 1, from, aux, to);
+			System.out.println(from + " move to " + to);
+			TowersOfHanoi(n -1, aux, to, from);
+		}
+	}
+	// 递归判断数组是否有序
+	public static int isSortedArray(int[] arr, int len){
+		if(len == 1){
+			return 1;
+		}
+		return (arr[len-1] < arr[len-2])? 0: isSortedArray(arr, len-1);
+	}
+	
+	public static void main(String[] args){
+		
+		// TowersOfHanoi(2, 'A', 'B', 'C');
+		
+		int[] arr = {1, 3, 5, 7, 6};
+		System.out.println(isSortedArray(arr, arr.length));
+	}
+}
